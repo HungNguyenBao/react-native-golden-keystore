95f5a854901da608cb35b635acd65cb5cd61f77b
Bảo Hưng <baohung2412@gmail.com>
2022-03-21 16:08:07 +0700

    first commit

---
+++ coinTypes.js
@@ -0,0 +1,20 @@
+const networks = {
+  BTC: {
+    name: 'bitcoin',
+    path: "m/49'/0'/0'/0/index",
+  },
+  ETH: {
+    name: 'ethereum',
+    path: "m/44'/60'/0'/0/index",
+  },
+  NEO: {
+    name: 'neo',
+    path: "m/44'/888'/0'/0/index",
+  },
+  LTC: {
+    name: 'litecoin',
+    path: "m/44'/2'/0'/0/index",
+  },
+};
+
+export default networks;
